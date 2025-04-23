344b888b8e450fcab427344a83c370cef501d393
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-04-23 08:28:35 +0530

    day 03

---
+++ day_03_april_23/addition.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 20;
+
+console.log(`Addition of ${a} and ${b} is ${a + b}`); // works in both browser & Nodejs
+console.log('Addition of '+a+' and '+b+' is '+(a+b)); 
+
+// alert(`Addition of ${a} and ${b} is ${a+b}`); // works only in browser and not in Nodejs
+// document.writeln(`Addition of ${a} and ${b} is ${a + b}`); // works only in browser and not in Nodejs
